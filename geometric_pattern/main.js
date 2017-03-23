bedd4fda6c015343a6eda4ec0fb31b49d461044a
Cayden Baker <cayden.baker@bridgeportschools.net>
2017-03-23 19:50:56 +0000

    Offset circles in geomoetric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,19 +1,32 @@
 var NUM_CIRCLES = 12;
 var circleDiameter;
+var circleRadius;
 
 function setup() {
      createCanvas(480, 600);
      circleDiameter = width/NUM_CIRCLES;
+     circleRadius = circleDiameter/2;
 }
 
 function draw() {
+     var isShifted = false;
+     
      var y = 0;
      while (y <= height) {
+          var x;
           
-          var x = 0;
-          while (x <= width) {
-          ellipse(x, 0, circleDiameter, circleDiameter)
-          x = x + circleDiameter;
+          if (isShifted) {
+               x = circleRadius;
+          } else {
+               x = 0;
           }
+          
+          while (x <= width) {
+               ellipse(x, 0, circleDiameter, circleDiameter)
+               x = x + circleDiameter;
+          }
+          
+          y = y + circleRadius;
+          isShifted = !isShifted;
      }
 }
