a80baf2fceb4aa46a336a76b87fe20938ad0376a
Cayden Baker <cayden.baker@bridgeportschools.net>
2017-04-13 19:25:44 +0000

    Find Bigfoot workshop

--- dodge/game.js
+++ dodge/game.js
@@ -1,23 +1,24 @@
 var player;
 var playerImage;
-var isGameOver;
 var enemy;
 var enemyImage;
-var BackgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png")
+var backgroundImage;
+var isGameOver;
 
 function preload() {
-     playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
      enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
+     backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png")
+     playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
 }
 
 function setup() {
-     isGameOver = false;
      createCanvas(256,256);
-     player = createSprite(width/2, height-(playerImage.height/2), 0, 0);
+     player = createSprite(width/2, height-25);
      player.addImage(playerImage);
-     enemy = createSprite(width/2, 0, 0, 0);
+     enemy = createSprite(width/2, 0);
      enemy.addImage(enemyImage);
      enemy.rotationSpeed = 4.0;
+     isGameOver = false;
 }
 
 function draw() {
@@ -31,15 +32,14 @@ function draw() {
           }
      
      
-     -background(0, 50, 100);
-     +background(backgroundImage);
+     background(backgroundImage);
      
-     if (keyDown(RIGHT_ARROW) && player.position.x < (width-25)) {
-          player.position.x = player.position.x + 1;
+     if (keyDown(RIGHT_ARROW) && player.position.x < (playerImage.width/2)) {
+          player.position.x = player.position.x += 2;
      }
      
-     if (keyDown(LEFT_ARROW) && player.position.x > 25) {
-          player.position.x = player.position.x - 1;
+     if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width/2)) {
+          player.position.x = player.position.x -= 2;
      }
      
      enemy.position.y = enemy.position.y + 3;
@@ -62,7 +62,6 @@ function gameOver() {
 }
 
 function mouseClicked() {
-     player.position.y = height-(playerImage.height/2);
      if (isGameOver) {
           isGameOver = false;
           player.position.x = width/2;
