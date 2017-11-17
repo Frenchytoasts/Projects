b31fd14ce1d30169612b3a4838b2508a0d94707f
Cayden Baker <cayden.baker@bridgeportschools.net>
2017-11-17 13:38:47 +0000

    chat

--- chat/main.js
+++ chat/main.js
@@ -1,12 +1,12 @@
-// var config = {
-//  apiKey: "AIzaSyB4OIcWiO4UFAcLh3LdG1NzIsdtVMJt0FY",
-//  authDomain: "chat-app-22349.firebaseapp.com",
-//  databaseURL: "https://chat-app-22349.firebaseio.com",
-//  projectId: "chat-app-22349",
-//  storageBucket: "chat-app-22349.appspot.com",
-// messagingSenderId: "297983316811"
-// };
-// firebase.initializeApp(config);
+ var config = {
+  apiKey: "AIzaSyB4OIcWiO4UFAcLh3LdG1NzIsdtVMJt0FY",
+  authDomain: "chat-app-22349.firebaseapp.com",
+  databaseURL: "https://chat-app-22349.firebaseio.com",
+  projectId: "chat-app-22349",
+  storageBucket: "chat-app-22349.appspot.com",
+  messagingSenderId: "297983316811"
+ };
+ firebase.initializeApp(config);
  
  var chatData = firebase.database().ref();
  
